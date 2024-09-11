fa9b2e72e72187772de1d36998206b9d42e665a3
cerdelen <cedric.erdelen@gmail.com>
2024-09-11 18:22:57 +0200

    Init Command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#[allow(unused_imports)]
+use std::env;
+use std::fs;
+
+use clap::{Parser, Subcommand};
+
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Init,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    println!("Logs from your program will appear here!");
+
+    // Uncomment this block to pass the first stage
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+    }
 }
