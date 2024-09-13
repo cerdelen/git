d0febb34e8fa2eb754d3388d5d36685c39d74665
cerdelen <cedric.erdelen@gmail.com>
2024-09-13 19:46:14 +0200

    comment

--- src/commands/write_tree.rs
+++ src/commands/write_tree.rs
@@ -9,6 +9,9 @@ pub(crate) fn invoke() ->anyhow::Result<()> {
     //      if file -> blob and store it in temp vec
     //      if dir recursively do all of this
     //      from temp vec with recursively read dirs and all blobs create tree obj
+    //      (find out how to sort this temp vec so the tree obj are right order and hashes are same
+    //      as in git)
+
 
     Ok(())
 }
